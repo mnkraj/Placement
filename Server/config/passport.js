6cffc0e0cbe49162042283287be3c62d77688042
Mayank Raj <mayankraj7100@gmail.com>
2025-02-11 15:46:52 +0530

    f

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser ((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: `${process.env.DEPLOYED_URL}/auth/google/callback`,
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id }).then(existingUser  => {
         const email = profile.emails[0].value;
