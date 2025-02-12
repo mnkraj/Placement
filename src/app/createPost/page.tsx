946436ff17ed5e2cb58834f236b0b06e761b48ce
Mayank Raj <mayankraj7100@gmail.com>
2025-02-13 03:40:54 +0530

    .

--- src/app/createPost/page.tsx
+++ src/app/createPost/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React , {useState} from 'react'
+import React  from 'react'
 import Tiptap from './Editor';
 const page = () => {
-  const [value, setValue] = useState('');
+  
   return (
     <div>
       <div className='mt-20 flex justify-center align-centre'>
