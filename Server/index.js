8a6d5913d41fc498f7b9609cb5e5e469f4369563
Mayank Raj <mayankraj7100@gmail.com>
2025-02-14 00:44:56 +0530

    .

--- Server/index.js
+++ Server/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const connectDB = require("./config/Db");
 const passport = require("passport");
 require("./config/passport")
+const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const authroutes = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
 
-
+app.use(cookieParser());
 app.use(cors({
   origin: `${process.env.FRONTEND_URL}`,
   methods: ["GET", "POST"],
@@ -27,6 +30,10 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URL,
+    ttl: 60 * 60, // 7 days
+}),
   cookie: {
     maxAge: 60 * 60 * 1000, 
     secure: true,
