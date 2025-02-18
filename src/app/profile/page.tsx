82fc85be5e0cc1d6923f160dc3429cb0763124ae
Mayank Raj <mayankraj7100@gmail.com>
2025-02-18 17:49:24 +0530

    fd

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Link from "next/link";
 import Spinner from "../components/Spinner";
 import { useRouter } from "next/navigation"; // Import useRouter
